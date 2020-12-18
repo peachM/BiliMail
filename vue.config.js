25e059d509cfafdab70735ffb20366adf7502510
peachM <1104234512@qq.com>
2020-12-18 17:31:16 +0800

    tabbar的封装和路由

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,13 @@
-module.exports = {}
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    }
+  }
+}
